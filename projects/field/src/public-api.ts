222c01896d0eee84f5bf9384c5a6017a80ec70e3
srgak <makuta5480@mail.ru>
2022-02-27 19:45:40 +0300

    Add files via upload

---
+++ projects/field/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of field
+ */
+
+export * from './lib/field.service';
+export * from './lib/field.component';
+export * from './lib/field.module';
+export * from './lib/components/input/input.component';
