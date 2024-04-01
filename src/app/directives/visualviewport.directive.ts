38507cb196888f7dde789705c774293c4a022ae2
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2024-04-01 09:59:57 +0300

    test

--- src/app/directives/visualviewport.directive.ts
+++ src/app/directives/visualviewport.directive.ts
@@ -1,22 +1,18 @@
 import { ChangeDetectorRef, Directive, HostBinding } from "@angular/core";
-import { filter, first, fromEvent, map } from "rxjs";
+import { fromEvent, map } from "rxjs";
 
 @Directive({
   selector: "[appVisualviewport]",
 })
 export class VisualviewportDirective {
-  @HostBinding("style.height") private height: string;
+  @HostBinding("style.height") public height: string;
 
   constructor(private cdr: ChangeDetectorRef) {
     fromEvent(window.visualViewport as VisualViewport, "resize")
-      .pipe(
-        map(() => window.visualViewport as VisualViewport),
-        filter((data) => data.height < window.innerHeight),
-        first()
-      )
-      .subscribe((data) => {
-        console.log(data.height);
-        this.height = `${data.height}px`;
+      .pipe(map(() => window.visualViewport as VisualViewport))
+      .subscribe(({ height }) => {
+        console.log(height);
+        this.height = `${height}px`;
         cdr.markForCheck();
       });
   }
