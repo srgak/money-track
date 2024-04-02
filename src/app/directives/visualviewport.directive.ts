5df11198f52affdc9a0212466664426c8d668480
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2024-04-02 12:40:14 +0300

    tedt

--- src/app/directives/visualviewport.directive.ts
+++ src/app/directives/visualviewport.directive.ts
@@ -22,7 +22,7 @@ export class VisualviewportDirective {
   ) {
     fromEvent(window.visualViewport as VisualViewport, "resize")
       .pipe(
-        debounceTime(50),
+        debounceTime(100),
         map(() => window.visualViewport as VisualViewport),
         tap(({ height }) => {
           this.isOpenKeyboard = height < window.innerHeight;
@@ -39,7 +39,7 @@ export class VisualviewportDirective {
       });
 
     fromEvent(window, "scroll")
-      .pipe(debounceTime(50))
+      .pipe(debounceTime(100))
       .subscribe(() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
       });
