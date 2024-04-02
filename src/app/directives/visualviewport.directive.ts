06feb6509968e0ba2c25586eebfbfdab838e0bb8
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2024-04-02 14:42:40 +0300

    test

--- src/app/directives/visualviewport.directive.ts
+++ src/app/directives/visualviewport.directive.ts
@@ -39,7 +39,7 @@ export class VisualviewportDirective {
       });
 
     fromEvent(window, "scroll")
-      .pipe(debounceTime(500))
+      .pipe(debounceTime(600))
       .subscribe(() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
       });
