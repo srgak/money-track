b5906804fbb3e3e2429cfd4362d938098a2b457f
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2024-04-04 12:41:25 +0300

    test

--- src/app/directives/correctCaret.directive.ts
+++ src/app/directives/correctCaret.directive.ts
@@ -1,18 +1,20 @@
 import { Directive, ElementRef, HostListener, OnInit } from "@angular/core";
 import { NgControl } from "@angular/forms";
+import { fromEvent } from "rxjs";
 
 @Directive({
   selector: "[correctCaret]",
 })
 export class CorrectCaret implements OnInit {
-  @HostListener("input") public moveCaret(): void {
-    const { length } = this.ngControl.control?.value;
+  private inputElement = this.elRef.nativeElement as HTMLInputElement;
+  // @HostListener("input") public moveCaret(): void {
+  //   const { length } = this.ngControl.control?.value;
 
-    (this.elRef.nativeElement as HTMLInputElement).setSelectionRange(
-      length,
-      length
-    );
-  }
+  //   (this.elRef.nativeElement as HTMLInputElement).setSelectionRange(
+  //     length,
+  //     length
+  //   );
+  // }
 
   // @HostListener("touchend") public touchEnd(): void {
   //   this.moveCaret();
@@ -24,11 +26,17 @@ export class CorrectCaret implements OnInit {
 
   constructor(private ngControl: NgControl, private elRef: ElementRef) {}
 
+  private moveCaret(): void {
+    const { length } = this.ngControl.control?.value;
+
+    this.inputElement.setSelectionRange(length, length);
+  }
+
   ngOnInit() {
+    fromEvent(this.inputElement, "input").subscribe(() => this.moveCaret());
     this.ngControl.control?.valueChanges.subscribe((value) => {
       if (!value.length) {
         this.ngControl.control?.reset("0");
-        // this.moveCaret();
       }
     });
   }
