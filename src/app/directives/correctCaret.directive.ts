2cf4231d59543e86b9a71ad65652c027e251fc75
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2024-04-04 11:37:01 +0300

    test

--- src/app/directives/correctCaret.directive.ts
+++ src/app/directives/correctCaret.directive.ts
@@ -5,31 +5,31 @@ import { NgControl } from "@angular/forms";
   selector: "[correctCaret]",
 })
 export class CorrectCaret implements OnInit {
-  @HostListener("click") public moveCaret(): void {
-    const { length } = this.ngControl.control?.value;
+  // @HostListener("click") public moveCaret(): void {
+  //   const { length } = this.ngControl.control?.value;
 
-    (this.elRef.nativeElement as HTMLInputElement).setSelectionRange(
-      length,
-      length
-    );
-  }
+  //   (this.elRef.nativeElement as HTMLInputElement).setSelectionRange(
+  //     length,
+  //     length
+  //   );
+  // }
 
   // @HostListener("touchend") public touchEnd(): void {
   //   this.moveCaret();
   // }
 
-  @HostListener("paste") public paste(): void {
-    this.moveCaret();
-  }
+  // @HostListener("paste") public paste(): void {
+  //   this.moveCaret();
+  // }
 
   constructor(private ngControl: NgControl, private elRef: ElementRef) {}
 
   ngOnInit() {
-    this.ngControl.control?.valueChanges.subscribe((value) => {
-      if (!value.length) {
-        this.ngControl.control?.reset("0");
-        this.moveCaret();
-      }
-    });
+    // this.ngControl.control?.valueChanges.subscribe((value) => {
+    //   if (!value.length) {
+    //     this.ngControl.control?.reset("0");
+    //     this.moveCaret();
+    //   }
+    // });
   }
 }
