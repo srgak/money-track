440c6e41ba92762ad80b1a4f1f4bd28ba5e44fe2
Sergey Akimov <sergeyakimov@MacBook-Pro-Sergey.local>
2024-04-13 10:03:58 +0300

    test

--- src/app/components/minutes-field/minutes-field.component.ts
+++ src/app/components/minutes-field/minutes-field.component.ts
@@ -85,5 +85,7 @@ export class MinutesFieldComponent
       });
   }
 
-  ngAfterViewInit(): void {}
+  ngAfterViewInit(): void {
+    (this.inputRef.nativeElement as HTMLInputElement).focus();
+  }
 }
